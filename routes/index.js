df36759dbb7b75ed3a347eff280029c019a936d8
whangwang <whangwang0430@gmail.com>
2017-12-28 01:59:06 +0800

    almost done

--- routes/index.js
+++ routes/index.js
@@ -7,14 +7,14 @@ module.exports = function(app) {
     app.get('/', function(req, res) {
         res.render('pages/index');
     });
-    app.get('/show', function(req, res, next) {
+    app.post('/ajax_search', function(req, res, next) {
         var bodys;
-
+        console.dir('search');
         var options = {
           "method": "GET",
           "hostname": "api.kkbox.com",
           "port": null,
-          "path": "/v1.1/search?q=Mayday&type=track&territory=TW&offset=0&limit=50",
+          "path": "/v1.1/search?q=Justin&nbsp;Bieber&type=track&territory=TW&offset=0&limit=10" ,
           "headers": {
             "accept": "application/json",
             "authorization": "Bearer 1KTq6xTBcZovkrFrB2+JUA=="
@@ -31,9 +31,51 @@ module.exports = function(app) {
           ress.on("end", function () {
             var body = Buffer.concat(chunks);
             bodys = JSON.parse(body);
-            res.render('pages/show',{
+          /*  res.render('pages/show',{
                 body: bodys
-            })
+            }) */
+            res.json({
+              msg: 'success',
+              data: bodys
+            });
+            //bodys = body;
+            console.dir(bodys);
+          });
+        });
+    /*    console.dir(bodys); */
+        req.end();
+    });
+    app.post('/ajax_search_album', function(req, res, next) {
+        var bodys;
+        console.dir('search');
+        var options = {
+          "method": "GET",
+          "hostname": "api.kkbox.com",
+          "port": null,
+          "path": "/v1.1/albums/Stp6OhOOyDQXwjFVtX/tracks?territory=TW&offset=0&limit=10" ,
+          "headers": {
+            "accept": "application/json",
+            "authorization": "Bearer 1KTq6xTBcZovkrFrB2+JUA=="
+          }
+        };
+        var bodys;
+        var req = http.request(options, function (ress) {
+          var chunks = [];
+
+          ress.on("data", function (chunk) {
+            chunks.push(chunk);
+          });
+
+          ress.on("end", function () {
+            var body = Buffer.concat(chunks);
+            bodys = JSON.parse(body);
+          /*  res.render('pages/show',{
+                body: bodys
+            }) */
+            res.json({
+              msg: 'success',
+              data: bodys
+            });
             //bodys = body;
             console.dir(bodys);
           });
